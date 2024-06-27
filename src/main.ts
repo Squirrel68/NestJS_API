a411b7717d59ffc76b956922fb47c7f3fd2304cc
Squirrel68 <constantlypeaceinside@gmail.com>
2024-06-27 14:36:38 +0700

    #5 - at add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yaml');
+const swaggerUi = require('swagger-ui-express');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const file = fs.readFileSync(path.resolve('timesheet-swagger.yaml'), 'utf8');
+  console.log(file);
+
+  const swaggerDocument = YAML.parse(file);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(3000);
 
   if (module.hot) {
